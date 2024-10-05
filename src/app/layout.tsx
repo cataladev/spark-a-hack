3b208cc62d5258c4a07d004aba6a9b9d64bc580b
MSI <lopez212318@gmail.com>
2024-10-05 17:27:35 -0400

    dark mode kind of

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ export const metadata: Metadata = {};
 import { Noto_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { HydrateClient } from "~/trpc/server";
+import { dark } from "@clerk/themes";
 
 // If loading a variable font, you don't need to specify the font weight
 const notoSans = Noto_Sans({
@@ -20,16 +21,22 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
     <html lang="en" className={notoSans.className}>
       <body>
-        <ClerkProvider>
+        
           <TRPCReactProvider>
             <HydrateClient>
             {children}
             </HydrateClient>
           </TRPCReactProvider>
-        </ClerkProvider>
+        
       </body>
     </html>
+    </ClerkProvider>
   );
 }
