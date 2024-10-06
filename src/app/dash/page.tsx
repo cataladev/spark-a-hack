1969943438cbd6d74db81527fcd18c4d671bef02
Daniel Efres <da947619@ucf.edu>
2024-10-05 20:31:44 -0400

    front 90 degree done

--- src/app/dash/page.tsx
+++ src/app/dash/page.tsx
@@ -9,7 +9,7 @@ export default async function Dashboard() {
   return (
     <>
       <NavBar />
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#969195] to-[#3C3744] text-white">
+      <main className="flex min-h-screen flex-col items-center justify-center gradient-background text-white">
         <div className="flex flex-col items-center justify-center">
           <h1 className="text-[#f1d302] bg-[#3C3744] mt-10 text-4xl font-bold rounded-full px-4 py-2">Dashboard</h1>
           <UserInputForm />
